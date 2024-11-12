0162f69d2fd98fd0b6a12bcdb12e1f33f32626b7
] <kamilaserikpaeva@MacBook-Air-kamila.local>
2024-11-12 17:48:25 +0500

    Сохранение изменений перед слиянием

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
-l
+
 db = SQLAlchemy()
 app = Flask(__name__)
 
